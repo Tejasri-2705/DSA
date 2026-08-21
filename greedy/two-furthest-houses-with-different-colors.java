87addb0858064546eaae2a73f17f7edd92029119
Maradani Tejasri <23a81a0537@sves.org.in>
2026-08-21 11:30:58 +0530

    Create two-furthest-houses-with-different-colors.java

---
+++ greedy/two-furthest-houses-with-different-colors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxDistance(int[] colors) {
+        
+        int n=colors.length;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(colors[n-1]!=colors[i])
+            {
+                ans=Math.max(ans,(n-1-i));
+                break;                
+            }
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            if(colors[0]!=colors[i])
+            {
+                ans=Math.max(ans,i);
+                break;                
+            }
+        }
+        return ans;
+    }
+}
